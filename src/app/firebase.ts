856d618a19fb2fabfb4b24547a847165ad99abfa
Liz Zhang <lizzhang.0509@gmail.com>
2021-08-26 23:21:00 +0100

    login and register working

---
+++ src/app/firebase.ts
@@ -0,0 +1,14 @@
+
+
+const config = {
+    apiKey: "AIzaSyBiUQmBYIQNkH1Bxi5WAckakIpVnuPbvos",
+    authDomain: "yolopet-finalproject.firebaseapp.com",
+    databaseURL: "https://yolopet-finalproject-default-rtdb.europe-west1.firebasedatabase.app",
+    projectId: "yolopet-finalproject",
+    storageBucket: "yolopet-finalproject.appspot.com",
+    messagingSenderId: "844521947975",
+    appId: "1:844521947975:web:3570d460bc4f1266abc262",
+    measurementId: "G-NVJB2V9LVH"
+  }
+
+  export default config
